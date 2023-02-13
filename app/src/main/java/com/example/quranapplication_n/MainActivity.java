f4433bf6c11535d6f189b0995efea452c3570434
Fajar Dar <Fajardar174@gmail.com>
2023-02-13 13:07:09 +0000

    Files For Arabic Text and Indexes Added

--- app/src/main/java/com/example/quranapplication_n/MainActivity.java
+++ app/src/main/java/com/example/quranapplication_n/MainActivity.java
@@ -11,49 +11,5 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView pText;
-    TextView aText;
-    Button search;
-    TextView display;
-    Button git;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        pText = findViewById(R.id.parahText);
-        aText = findViewById(R.id.ayatText);
-        search = findViewById(R.id.buttonSearch);
-        display = findViewById(R.id.displayWindow);
-        git = findViewById(R.id.buttonGit2);
-        search.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                int valP = Integer.parseInt(pText.getText().toString());
-                int valA = Integer.parseInt(aText.getText().toString());
-                QDH qdh=new QDH();
-                int ps=qdh.getParahStart(valP-1);
-                ps+=valA;
-                QuranArabicText txt=new QuranArabicText();
-                String ayat=txt.QuranArabicText[ps];
-                display.setText(ayat);
-
-            }
-        });
-
-        git.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String url = "";
-
-                Uri webpage = Uri.parse(url);
-                Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
-
-                startActivity(intent);
-            }
-        });
-
-
-    }
+    
 }
